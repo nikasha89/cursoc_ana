9d5c771b18e132370e91259500c2af3d76adb526
nikasha <nikasha_moi@hotmail.com>
2016-04-03 20:51:39 +0200

    Ejercico 1 Tema 3

---
+++ programas/ejercicios/tema3/ejercicio1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ( )
+{
+ int entero = 3 ;
+ char c ;
+ c = 'c' ;
+ printf("int \n\t valor = %d \n\t tamanyo %d \n\n", entero, sizeof(entero));
+ printf("char \n\t valor= %c \n\t tamanyo %d \n\n", c , sizeof(char));
+}

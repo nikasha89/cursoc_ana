2a10c371161fa099741492c5a9f9fbc8c57eebac
nikasha <nikasha_moi@hotmail.com>
2016-04-03 21:12:15 +0200

    Modificaciones ejercicio1

--- programas/ejercicios/tema3/ejercicio1.c
+++ programas/ejercicios/tema3/ejercicio1.c
@@ -5,6 +5,13 @@ int main ( )
  int entero = 3 ;
  char c ;
  c = 'c' ;
+ long long int m = 123987234129341234;
+ unsigned char d = 32;
+ float decimal = 0.123;
  printf("int \n\t valor = %d \n\t tamanyo %d \n\n", entero, sizeof(entero));
  printf("char \n\t valor= %c \n\t tamanyo %d \n\n", c , sizeof(char));
+ printf("------ \n long long int\n\n valor= %lli \n\t tamanyo %d \n", m , sizeof(m));
+ printf("------ \n unsigned char\n\n valor= %d \n\t tamanyo %d \n", d, sizeof(d));
+ printf("------ \n float\n\n valor= %f \n\t tamanyo %d \n", decimal , sizeof(decimal));
+ 
 }

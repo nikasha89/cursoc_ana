98e16ff6a0223bab258d7366c5a89232624e6b49
nikasha89 <nikasha89@gmail.com>
2016-04-08 11:58:15 +0200

    Update hdr.h

--- juegoDeLaVida/hdr.h
+++ juegoDeLaVida/hdr.h
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/* Tamaño del array regular bidimensaional */
 #define TAM_ARRAY 50
+/* Número de iteraciones */
 #define ITERACION 60
+/* Lindantes se refiere a las 8 células lindantes que tiene cada una como máximo. */
+#define LINDANTES 8
 
 enum estado_celula{
 	VIVA, 
@@ -9,16 +13,24 @@ enum estado_celula{
 } EstadoCelula;
 
 struct celula{
-	EstadoCelula estado = MUERTA;
+	enum estado_celula estado = MUERTA;
 } Celula;
 
+struct coordenadas{
+	int x;
+	int y;
+} Coordenadas;
+
 struct tablero{
-	Celula tablero[TAM_ARRAY][TAM_ARRAY];
+	struct celula tablero[TAM_ARRAY][TAM_ARRAY];
 	int iteracion=0;
-	Celula tablero_inicial[TAM_ARRAY][TAM_ARRAY];
+	struct celula tablero_inicial[TAM_ARRAY][TAM_ARRAY];
+	struct coordenadas arrayCelulasVivas[TAM_ARRAY*TAM_ARRAY];
+	struct coordenadas arrayCelulasNacen[TAM_ARRAY*TAM_ARRAY];
+	struct coordenadas arrayCelulasMueren[TAM_ARRAY*TAM_ARRAY];
 } Tablero;
 
 int menuInicio();
-void iterarTablero(Tablero **t);
-void imprimeTablero(Tablero **t);
-void inicializarTablero(Tablero **t);
+void iterarTablero(struct tablero **t);
+void imprimeTablero(struct tablero **t);
+void inicializarTablero(struct tablero **t);
